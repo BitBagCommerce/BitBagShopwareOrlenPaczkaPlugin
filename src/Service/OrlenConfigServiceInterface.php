d2fe5d451086b1ce3d8f5550d56358bd42bffde6
Jakub Groncki <jakub.groncki@bitbag.pl>
2022-08-02 10:59:31 +0200

    Fix wrong credential names, add tests

--- src/Service/OrlenConfigServiceInterface.php
+++ src/Service/OrlenConfigServiceInterface.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace BitBagShopwareOrlenPaczkaPlugin\Config;
+namespace BitBagShopwareOrlenPaczkaPlugin\Service;
 
 use BitBagShopwareOrlenPaczkaPlugin\Model\OrlenApiConfig;
 
