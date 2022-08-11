82bbdb08c79ab7df6bd6eae7a4292ca99595f6f6
Jakub Groncki <jakub.groncki@bitbag.pl>
2022-08-11 16:47:42 +0200

    ecs

--- src/Controller/Api/CredentialsController.php
+++ src/Controller/Api/CredentialsController.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace BitBagShopwareOrlenPaczkaPlugin\Controller\Api;
 
-use BitBagShopwareOrlenPaczkaPlugin\Config\OrlenApiConfigServiceInterface;
 use BitBagShopwareOrlenPaczkaPlugin\Exception\InvalidApiConfigException;
 use BitBagShopwareOrlenPaczkaPlugin\Model\OrlenApiConfig;
 use BitBagShopwareOrlenPaczkaPlugin\Resolver\PPClientResolverInterface;
