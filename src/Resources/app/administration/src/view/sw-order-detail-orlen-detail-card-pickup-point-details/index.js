9e67a058c1eeefc1c36912404992ff2ff7d8450f
Norbert Glanc <norglanc@gmail.com>
2022-08-11 12:13:53 +0200

    Fix render properly pickup point data

--- src/Resources/app/administration/src/view/sw-order-detail-orlen-detail-card-pickup-point-details/index.js
+++ src/Resources/app/administration/src/view/sw-order-detail-orlen-detail-card-pickup-point-details/index.js
@@ -5,56 +5,37 @@ const { Component } = Shopware;
 Component.register('sw-order-detail-orlen-detail-card-pickup-point-details', {
     template,
     props: [
-        'order'
+        'order',
     ],
+    data() {
+        return {
+            showDetails: false,
+            pickupPoint: {
+                name: '',
+                street: '',
+                zipCode: '',
+                city: '',
+                province: '',
+            },
+        };
+    },
     created() {
         const order = this.order;
-
-        this.removeOrlenDetailCardIfNotFoundOrlen(order);
-
-        setTimeout(() => {
+        if (order && order.extensions.orlen && order.extensions.orlen.id) {
             this.setOrlenDetailsData(order);
-        }, 1000);
+
+            this.showDetails = true;
+        }
     },
     methods: {
         setOrlenDetailsData(order) {
             const data = order.extensions.orlen;
-            if (!data || !data.id) {
-                return;
-            }
 
-            const pointNameEl = this.$refs.pointName;
-            if (pointNameEl) {
-                pointNameEl.textContent = data.pickupPointName;
-            }
-
-            const streetEl = this.$refs.street;
-            if (streetEl) {
-                streetEl.textContent = data.pickupPointStreet;
-            }
-
-            const postCodeEl = this.$refs.postCode;
-            if (postCodeEl) {
-                postCodeEl.textContent = data.pickupPointZipCode;
-            }
-
-            const cityEl = this.$refs.city;
-            if (cityEl) {
-                cityEl.textContent = data.pickupPointCity;
-            }
-
-            const provinceEl = this.$refs.province;
-            if (provinceEl) {
-                provinceEl.textContent = data.pickupPointProvince;
-            }
-        },
-        removeOrlenDetailCardIfNotFoundOrlen(order) {
-            if (!order || !order.extensions.orlen || !order.extensions.orlen.id) {
-                const orlenDetailCardEl = this.$refs.orlenDetailsCard;
-                if (orlenDetailCardEl) {
-                    orlenDetailCardEl.remove();
-                }
-            }
+            this.pickupPoint.name = data.pickupPointName;
+            this.pickupPoint.street = data.pickupPointStreet;
+            this.pickupPoint.zipCode = data.pickupPointZipCode;
+            this.pickupPoint.city = data.pickupPointCity;
+            this.pickupPoint.province = data.pickupPointProvince;
         },
     }
 });
