0db092eb323e06e85d56d1d272137d127240e8aa
Norbert Glanc <norglanc@gmail.com>
2022-08-23 09:19:42 +0200

    Add information about this file has been created by developers from BitBag

--- tests/Config/OrlenApiConfigServiceTest.php
+++ tests/Config/OrlenApiConfigServiceTest.php
@@ -1,8 +1,14 @@
 <?php
 
+/*
+ * This file has been created by developers from BitBag.
+ * Feel free to contact us once you face any issues or want to start.
+ * You can find more information about us on https://bitbag.io and write us an email on hello@bitbag.io.
+ */
+
 declare(strict_types=1);
 
-namespace BitBag\ShopwareOrlenPaczkaPlugin\Tests\Service;
+namespace BitBag\ShopwareOrlenPaczkaPlugin\Tests\Config;
 
 use BitBag\ShopwareOrlenPaczkaPlugin\Config\OrlenApiConfigService;
 use BitBag\ShopwareOrlenPaczkaPlugin\Exception\InvalidApiConfigException;
